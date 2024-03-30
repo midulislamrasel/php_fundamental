77bfb06baa2f4abdb7641e155d86c8788353d38b
midulislamrasel <midulraddy121@gmail.com>
2024-03-30 21:39:43 +0600

    Add

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+//  $total=0;
+//    for($i=0;$i<8;$i++){
+//       $total=$total+$i;
+//    }
+//     echo $total . "\n";
+
+  $i=0;
+while($i<=10){
+   if($i<=5){
+    continue;
+   }
+   print($i);
+   $i=$i+1;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
